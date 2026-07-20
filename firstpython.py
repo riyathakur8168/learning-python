f1ddd1acfe8d3c9e92de7dfabdaf680c3776cbb0
riyathakur8168 <riyathakur81143@gmail.com>
2026-07-20 21:40:00 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -83,5 +83,5 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("hey how can i help you")
+engine.say("hey how may i help you")
 engine.runAndWait()'''
